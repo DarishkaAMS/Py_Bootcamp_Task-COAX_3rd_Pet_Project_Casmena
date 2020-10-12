f18da67659d8dfd1bc52a53786a5dbef67fd0101
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2020-10-12 11:33:15 +0300

    Add Blog Post page

--- mysic_blog/urls.py
+++ mysic_blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 # from mysic_blog import views -without class
-from mysic_blog.views import HomeView, ArticleDetailView
+from mysic_blog.views import HomeView, ArticleDetailView, AddPostView
 
 urlpatterns = [
     # path('', views.home, name="home") - without class
     path('', HomeView.as_view(), name='home'),
     path('article/<int:pk>', ArticleDetailView.as_view(), name='article_detail'),
+    path('add_post/', AddPostView.as_view(), name='add_post'),
 ]
