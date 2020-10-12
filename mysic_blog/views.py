138865559897d3f50aed431d6aa71e5ab186118f
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2020-10-12 16:05:59 +0300

    AddAdd like button/function

--- mysic_blog/views.py
+++ mysic_blog/views.py
@@ -27,7 +27,13 @@ class ArticleDetailView(DetailView):
         context = super(ArticleDetailView, self).get_context_data(*args, **kwargs)
         obj = get_object_or_404(Post, id=self.kwargs['pk'])
         total_likes = obj.total_likes()
+
+        liked = False
+        if obj.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
         context['total_likes'] = total_likes
+        context['liked'] = liked
         return context
 
 
@@ -52,5 +58,11 @@ class DeletePostView(DeleteView):
 
 def like_view(request, pk):
     post = get_object_or_404(Post, id=request.POST.get('post_id'))
-    post.likes.add(request.user)
+    liked = False
+    if post.likes.filter(id=request.user.id).exists():
+        post.likes.remove(request.user)
+        liked = False
+    else:
+        post.likes.add(request.user)
+        liked = True
     return HttpResponseRedirect(reverse('article_detail', args=[str(pk)]))
